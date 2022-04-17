ded60a7794a723e1badc5e2f52ba00bc55d71bbc
Markton <jmolarte@up.edu.ph>
2022-04-18 01:57:38 +0800

    Create read_img.py
    Created a read_img.py that displays the (.jpg) images in the sample_img folder.
    
    Note that oversized photos will not be displayed properly.

---
+++ Basics/Reading Images and Video/read_img.py
@@ -0,0 +1,22 @@
+# Reading an image using openCV
+# This program displays the images in the sample image directory one-by-one
+import cv2 as cv
+
+to_display = [] # List to store images to display
+
+
+# Display images in the sample images folder one-by-one
+for img_num in range(1, 3):
+    # Read an image using imread()
+    # syntax and parameters: cv.imread(path, flags)
+    path = f"sample_img/{img_num}_img.jpg" # Make sure images are in .jpg format
+    img = cv.imread(path)
+    to_display.append(img)
+
+for img_num in range(len(to_display)):
+    # To show the image use: imshow()
+    # syntax and parameters: cv.imshow(window_name, image)
+    cv.imshow(f'Sample Image {img_num}: Cat', to_display[img_num])
+
+    # wait for any key to be pressed, then the next image in to_display will be displayed
+    cv.waitKey(0)
