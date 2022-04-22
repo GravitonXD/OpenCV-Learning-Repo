f95b49c233bfa4309b5b56f78a9bc903c79701c2
Markton <jmolarte@up.edu.ph>
2022-04-22 15:53:25 +0800

    Update meteor.py
    Code refactoring

--- Basics/Self Exercises/meteor.py
+++ Basics/Self Exercises/meteor.py
@@ -1,7 +1,6 @@
 # This script is an exercise for moving shapes
 # An implementation of the learnings from reading a video, and drawing of shapes.
 # by JOHN MARKTON OLARTE
-
 import cv2 as cv
 import numpy as np
 import random as rnd
@@ -13,7 +12,10 @@ BLUE_COLOR = 255,0,0
 YELLOW_COLOR = 0,255,255
 RED_COLOR = 0,0,255
 
-def meteorite_color(speed):
+def create_canvas(width, height):
+    return np.zeros((height, width, 3), dtype="uint8")
+
+def meteoroid_color(speed):
     if 1 <= speed <= 3:
         return BLUE_COLOR
     elif 4 <= speed <= 6:
@@ -25,20 +27,18 @@ def meteorite_color(speed):
 
 def main():
     # Create a blank animation_canvas
-    animation_canvas = np.zeros((300, 300, 3), dtype="uint8")
-    speed_iter_canvas = np.zeros((50, 300, 3), dtype="uint8")
+    animation_canvas = create_canvas(300, 300)
+    speed_iter_canvas = create_canvas(300, 50)
 
     # initialized position of the x & y coordinates, iteration#, and speed of the moving ball
     position_x = animation_canvas.shape[1]//2
     position_y = 0
     speed = rnd.randint(1,10)
     iterations = 1
-    
-    # Initialize the color of the meteorite based on its speed
 
     # Draw a white circle on the animation_canvas that will move in y based on the speed variable
     while True:
-         # display the speed and iteration
+        # display the speed and iteration
         cv.putText(speed_iter_canvas, f"Speed: {speed} || Iterations: {iterations}", (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (RED_COLOR), 1)
         cv.putText(speed_iter_canvas, "Press 'p' to pause", (20, 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (RED_COLOR), 1)
         cv.imshow("Speed & Iteration Counter", speed_iter_canvas)
@@ -49,8 +49,8 @@ def main():
             # reset position of the y coordinate
             position_y = 0
             # reset both canvasses
-            animation_canvas = np.zeros((300, 300, 3), dtype="uint8")
-            speed_iter_canvas = np.zeros((50, 300, 3), dtype="uint8")
+            animation_canvas = create_canvas(300, 300)
+            speed_iter_canvas = create_canvas(300, 50)
             # change speed, make sure it is not the same as the previous speed
             new_speed = rnd.randint(1,10)
             speed = new_speed if new_speed != speed else speed
@@ -58,14 +58,14 @@ def main():
         # draw a white circle on the animation_canvas
         cv.circle(animation_canvas, (position_x, position_y), 10, (WHITE_COLOR), cv.FILLED)
         # show the circle in the animation_canvas
-        cv.imshow('Meteorite', animation_canvas)
+        cv.imshow('Meteoroid', animation_canvas)
 
         # increment the y coordinate
         position_y += speed
         # to simulate the movement of the circle, add a black circle on top of the previous white circle.
         cv.circle(animation_canvas, (position_x, position_y-speed), 10, (BLACK_COLOR), cv.FILLED)
-        # simulate a lightening effect on the meteorite based on the speed
-        circle_blur = cv.circle(animation_canvas, (position_x, position_y-speed), 10, (meteorite_color(speed)), cv.FILLED)
+        # simulate a lightening effect on the meteoroid based on the speed
+        circle_blur = cv.circle(animation_canvas, (position_x, position_y-speed), 10, (meteoroid_color(speed)), cv.FILLED)
         cv.GaussianBlur(circle_blur, (0,0), 10, circle_blur)
 
         # Press 'p' to pause the animation
